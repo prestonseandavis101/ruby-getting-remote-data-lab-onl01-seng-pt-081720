fedd893ca7f119b995f6bdebc805cf1d3170f7ea
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-27 15:35:34 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,17 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+
+class GetRequester
+  
+ def initialize(url) 
+   url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+ end
+  
+  def get_response_body
+  end
+    
+
+  
+end
