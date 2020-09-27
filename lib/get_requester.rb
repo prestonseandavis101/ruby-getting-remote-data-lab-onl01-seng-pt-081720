5063eda97b0cae562f26483b7549c09dcc2bf659
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-27 15:51:02 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,7 +6,7 @@ require 'json'
 class GetRequester
   
  def initialize(url) 
-   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+   url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
  end
   
   def get_response_body
